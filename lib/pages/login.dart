ad36a69ae339b7944e0db8bedcd35ec82887df45
Banana <bananachristian04@gmail.com>
2025-01-06 12:27:03 +0300

    Initial commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -13,98 +13,100 @@ class LoginPage extends StatelessWidget {
     final TextEditingController usernameController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
     return Scaffold(
-        body: SingleChildScrollView(
-          child:Container(
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.center,
-            child:Column(
-              children: [
-              //Login form
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding:const EdgeInsets.all(20),
-                  color:Theme.of(context).colorScheme.primary,
-                  child:Column(
-                    children: [
-                      //Logo section
-                      const SizedBox(
-                        width:200,
-                        child: Image(image:AssetImage('assets/logo/podvibes_bgless.png'))
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child:Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              child:Column(
+                children: [
+                //Login form
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding:const EdgeInsets.all(20),
+                    color:Theme.of(context).colorScheme.primary,
+                    child:Column(
+                      children: [
+                        //Logo section
+                        const SizedBox(
+                          width:200,
+                          child: Image(image:AssetImage('assets/logo/podvibes_bgless.png'))
+                          ),
+                        //Main text
+                        Text(
+                          'Login',
+                          style:TextStyle(
+                            color:Theme.of(context).colorScheme.inversePrimary,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                          )
+                          ),
+                        const SizedBox(height:20),
+                        //Username
+                        Fields(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          hintText: 'Username', 
+                          obscureText: false, 
+                          controller: usernameController, 
+                          icon: const Icon(Icons.person)
+                          ),
+                        const SizedBox(height:20),
+                        //Password
+                        Fields(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          hintText: 'password', 
+                          obscureText: true, 
+                          controller: passwordController, 
+                          icon: const Icon(Icons.key)
                         ),
-                      //Main text
-                      Text(
-                        'Login',
-                        style:TextStyle(
-                          color:Theme.of(context).colorScheme.inversePrimary,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold
-                        )
+                        const SizedBox(height:20),
+                        //Forgot Password
+                        Text(
+                          'Forgot password?',
+                          style:TextStyle(
+                            color:Theme.of(context).colorScheme.inversePrimary
+                          )
                         ),
-                      const SizedBox(height:20),
-                      //Username
-                      Fields(
-                        color: Theme.of(context).colorScheme.inversePrimary, 
-                        hintText: 'Username', 
-                        obscureText: false, 
-                        controller: usernameController, 
-                        icon: const Icon(Icons.person)
+                        const SizedBox(height:20),
+                        //Buttons
+                        MyButtons(
+                          color: Colors.amber, 
+                          text: 'Login', 
+                          textColor: Theme.of(context).colorScheme.inversePrimary, 
+                          onTap: (){}
                         ),
-                      const SizedBox(height:20),
-                      //Password
-                      Fields(
-                        color: Theme.of(context).colorScheme.inversePrimary, 
-                        hintText: 'password', 
-                        obscureText: true, 
-                        controller: passwordController, 
-                        icon: const Icon(Icons.key)
-                      ),
-                      const SizedBox(height:20),
-                      //Forgot Password
-                      Text(
-                        'Forgot password?',
-                        style:TextStyle(
-                          color:Theme.of(context).colorScheme.inversePrimary
-                        )
-                      ),
-                      const SizedBox(height:20),
-                      //Buttons
-                      MyButtons(
-                        color: Colors.amber, 
-                        text: 'Login', 
-                        textColor: Theme.of(context).colorScheme.inversePrimary, 
-                        onTap: (){}
-                      ),
-                      const SizedBox(height:10),
-                      //Questions
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children:[
-                            GestureDetector(
-                              onTap:(){
-                                togglePages();
-                              },
-                              child: const Text(
-                                'Do not have an account? Register',
-                                style:TextStyle(color:Colors.amber)
-                                ),
-                            ),
-                            GestureDetector(
-                              onTap:(){
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context)=>const Home())
-                                );
-                              },
-                              child: const Text('Continue as guest')
-                              )
-                          ],
-                        ),
-                    ],
-                    )
-                ),
-              )
-              ],
+                        const SizedBox(height:10),
+                        //Questions
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children:[
+                              GestureDetector(
+                                onTap:(){
+                                  togglePages();
+                                },
+                                child: const Text(
+                                  'Register',
+                                  style:TextStyle(color:Colors.amber)
+                                  ),
+                              ),
+                              GestureDetector(
+                                onTap:(){
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context)=>const Home())
+                                  );
+                                },
+                                child: const Text('Continue as guest')
+                                )
+                            ],
+                          ),
+                      ],
+                      )
+                  ),
+                )
+                ],
+              ),
             ),
           ),
         ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -20,110 +20,112 @@ class RegisterPage extends StatelessWidget {
     final TextEditingController confirmPasswordController=TextEditingController();
 
     return Scaffold(
-      body:SingleChildScrollView(
-          child:Container(
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.center,
-            child:Column(
-              children: [
-              //Login form
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding:const EdgeInsets.all(20),
-                  color:Theme.of(context).colorScheme.primary,
-                  child:Column(
-                    children: [
-                      //Logo section
-                      const SizedBox(
-                        width:200,
-                        child: Image(image:AssetImage('assets/logo/podvibes_bgless.png'))
+      body:SafeArea(
+        child: SingleChildScrollView(
+            child:Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.center,
+              child:Column(
+                children: [
+                //Login form
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding:const EdgeInsets.all(20),
+                    color:Theme.of(context).colorScheme.primary,
+                    child:Column(
+                      children: [
+                        //Logo section
+                        const SizedBox(
+                          width:200,
+                          child: Image(image:AssetImage('assets/logo/podvibes_bgless.png'))
+                          ),
+                        //Main text
+                        Text(
+                          'Register',
+                          style:TextStyle(
+                            color:Theme.of(context).colorScheme.inversePrimary,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                          )
+                          ),
+                        const SizedBox(height:20),
+                        //Username
+                        Fields(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          hintText: 'Username', 
+                          obscureText: false, 
+                          controller: usernameController, 
+                          icon: const Icon(Icons.person)
+                          ),
+                        const SizedBox(height:20),
+                        //Email
+                        Fields(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          hintText: 'E-mail address', 
+                          obscureText: false, 
+                          controller: emailController, 
+                          icon: const Icon(Icons.email)
                         ),
-                      //Main text
-                      Text(
-                        'Register',
-                        style:TextStyle(
-                          color:Theme.of(context).colorScheme.inversePrimary,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold
-                        )
+                        const SizedBox(height:20),
+                        //Password
+                        Fields(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          hintText: 'password', 
+                          obscureText: true, 
+                          controller: passwordController, 
+                          icon: const Icon(Icons.key)
                         ),
-                      const SizedBox(height:20),
-                      //Username
-                      Fields(
-                        color: Theme.of(context).colorScheme.inversePrimary, 
-                        hintText: 'Username', 
-                        obscureText: false, 
-                        controller: usernameController, 
-                        icon: const Icon(Icons.person)
+                        const SizedBox(height:20),
+                        //Confirm password
+                        Fields(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          hintText: 'Confirm password', 
+                          obscureText: true, 
+                          controller: confirmPasswordController, 
+                          icon: const Icon(Icons.key)
+                        ),                  
+                        const SizedBox(height:20),
+                        //Buttons
+                        MyButtons(
+                          color: Colors.amber, 
+                          text: 'Register', 
+                          textColor: Theme.of(context).colorScheme.inversePrimary, 
+                          onTap: (){}
                         ),
-                      const SizedBox(height:20),
-                      //Email
-                      Fields(
-                        color: Theme.of(context).colorScheme.inversePrimary, 
-                        hintText: 'E-mail address', 
-                        obscureText: false, 
-                        controller: emailController, 
-                        icon: const Icon(Icons.email)
-                      ),
-                      const SizedBox(height:20),
-                      //Password
-                      Fields(
-                        color: Theme.of(context).colorScheme.inversePrimary, 
-                        hintText: 'password', 
-                        obscureText: true, 
-                        controller: passwordController, 
-                        icon: const Icon(Icons.key)
-                      ),
-                      const SizedBox(height:20),
-                      //Confirm password
-                      Fields(
-                        color: Theme.of(context).colorScheme.inversePrimary, 
-                        hintText: 'Confirm password', 
-                        obscureText: true, 
-                        controller: confirmPasswordController, 
-                        icon: const Icon(Icons.key)
-                      ),                  
-                      const SizedBox(height:20),
-                      //Buttons
-                      MyButtons(
-                        color: Colors.amber, 
-                        text: 'Register', 
-                        textColor: Theme.of(context).colorScheme.inversePrimary, 
-                        onTap: (){}
-                      ),
-                      const SizedBox(height:10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children:[
-                            GestureDetector(
-                              onTap:(){
-                                togglePages();
-                              },
-                              child: const Text(
-                                'Already have an account? Login',
-                                style:TextStyle(color:Colors.amber)
-                                ),
-                            ),
-                            GestureDetector(
-                              onTap:(){
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context)=>const Home())
-                                );
-                              },
-                              child: const Text('Continue as guest')
-                              )
-                          ],
-                        ),
-                    ],
-                    )
-                ),
-              )
-              ],
+                        const SizedBox(height:10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children:[
+                              GestureDetector(
+                                onTap:(){
+                                  togglePages();
+                                },
+                                child: const Text(
+                                  'Login',
+                                  style:TextStyle(color:Colors.amber)
+                                  ),
+                              ),
+                              GestureDetector(
+                                onTap:(){
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context)=>const Home())
+                                  );
+                                },
+                                child: const Text('Continue as guest')
+                                )
+                            ],
+                          ),
+                      ],
+                      )
+                  ),
+                )
+                ],
+              ),
             ),
           ),
-        ),
+      ),
     );
   }
 }
